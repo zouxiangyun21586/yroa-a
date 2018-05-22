a255aed54ce0610fae7f58601c5d606c6d5f8fa9
qianyigit <2442051156@qq.com>
2018-05-22 19:28:18 +0800

    新增一些页面

--- src/main/webapp/WEB-INF/admin/js/class/classAdd.js
+++ src/main/webapp/WEB-INF/admin/js/class/classAdd.js
@@ -41,7 +41,8 @@ layui.use([ 'layer', 'form' ], function() {
 							});
 						}, 1000);
 					}
-				},error : function() {
+				},
+				error : function() {
 					setTimeout(function() {
 						top.layer.close(index);
 						top.layer.msg("异常！", {
@@ -54,23 +55,24 @@ layui.use([ 'layer', 'form' ], function() {
 		}
 		return false;
 	});
-	form.verify({
-	    username: function(value, item){ //value：表单的值、item：表单的DOM对象
-	    if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)){
-	      return '用户名不能有特殊字符';
-	    }
-	    if(/(^\_)|(\__)|(\_+$)/.test(value)){
-	      return '用户名首尾不能出现下划线\'_\'';
-	    }
-	    if(/^\d+\d+\d$/.test(value)){
-	      return '用户名不能全为数字';
-	    }
-	    if(/^[\S]{6,12}$/.test(value)){
-	    	return '用户名必须6到12位，且不能出现空格';
-	    }
-	    },password: [
-	        /^[\S]{6,12}$/,
-	        '密码必须6到12位，且不能出现空格'
-	    ] 
-	});
+	 form.verify({
+   	  username: function(value, item){ //value：表单的值、item：表单的DOM对象
+   	    if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)){
+   	      return '用户名不能有特殊字符';
+   	    }
+   	    if(/(^\_)|(\__)|(\_+$)/.test(value)){
+   	      return '用户名首尾不能出现下划线\'_\'';
+   	    }
+   	    if(/^\d+\d+\d$/.test(value)){
+   	      return '用户名不能全为数字';
+   	    }
+   	    if(/^[\S]{6,12}$/.test(value)){
+   	    	return '用户名必须6到12位，且不能出现空格';
+   	    }
+   	  }
+   	  ,pass: [
+   	    /^[\S]{6,12}$/
+   	    ,'密码必须6到12位，且不能出现空格'
+   	  ] 
+   	});
 });
