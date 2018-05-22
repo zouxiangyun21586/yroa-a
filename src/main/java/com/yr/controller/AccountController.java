ebf642542075853cd4220680d57b96644ae3386c
zhouyehaogit2498 <2498230018@qq.com>
2018-05-22 11:56:05 +0800

    炸天帮·钢铁侠

---
+++ src/main/java/com/yr/controller/AccountController.java
@@ -0,0 +1,17 @@
+package com.yr.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 用户Conntroller
+ * @author 周业好
+ * 2018年5月22日 上午10:34:00
+ */
+@Controller
+public class AccountController {
+
+	public AccountController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
