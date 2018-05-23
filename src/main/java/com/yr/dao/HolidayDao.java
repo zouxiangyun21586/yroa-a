3dd234d7597730b0b8d10e582794bdf45f924c20
Linshuiqiaolj <2452750530@qq.com>
2018-05-23 20:00:41 +0800

    添加假期Dao层

--- src/main/java/com/yr/dao/HolidayDao.java
+++ src/main/java/com/yr/dao/HolidayDao.java
@@ -1,10 +1,20 @@
 package com.yr.dao;
 
+import com.yr.entity.Holiday;
+
 /**
  * 假期Dao
  * @作者 林水桥
  * 2018年5月22日下午10:26:49
  */
 public interface HolidayDao {
+	
+	/**
+	 * 添加假期
+	 * @param holiday 假期实体类
+	 * @return Integer 添加ID
+	 * 2018年5月23日下午7:57:59
+	 */
+	Integer addHoliday(Holiday holiday);
 	
 }
