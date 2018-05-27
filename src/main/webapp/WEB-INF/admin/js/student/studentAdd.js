618a86612e2a503306c42db4f2f4fa89d3dcdeb4
唐子壕 <@192.168.1.103>
2018-05-27 10:49:23 +0800

    tzh-学生管理

--- src/main/webapp/WEB-INF/admin/js/student/studentAdd.js
+++ src/main/webapp/WEB-INF/admin/js/student/studentAdd.js
@@ -47,9 +47,48 @@ layui.use([ 'layer', 'form' ,'laydate'], function() {
         }
     });
 	
+	
 	form.on('select(year)', function(data){
 		 $(":select[name='year']").val(data.value);
 	});   
+	
+	form.on('radio(isFinish)', function(data){
+		var i ="";
+		var a ="";
+		if(data.value=="0"){
+			
+		}else if(data.value=="1"){
+			i+="<div class='magb15 layui-col-md4 layui-col-xs12'>" +
+						"<label class='layui-form-label'>毕业日期</label>" +
+						"<div class='layui-input-block'>" +
+							"<input type='text' class='layui-input' id='finishTime' placeholder='yyyy-MM-dd' lay-verify='required' name='finishTime'>" +
+						"</div>" +
+					 "</div>" +
+					 "<div class='magb15 layui-col-md4 layui-col-xs12'>" +
+						 "<label class='layui-form-label'>工作日期</label>" +
+						 "<div class='layui-input-block userSex'>" +
+						  	 "<input type='text'  class='layui-input' id='offerTime' placeholder='yyyy-MM-dd' lay-verify='required' name='offerTime'>" +
+						 "</div>" +
+					"</div>"
+			 a+="<div class='layui-form-item layui-row layui-col-xs12'>" +
+					 "<div class='magb15 layui-col-md4 layui-col-xs12'>" +
+						"<label class='layui-form-label'>工资 </label>" +
+						"<div class='layui-input-block'>" +
+							"<input class='layui-input' placeholder='请输入你的工资' name='offerIncome' lay-verify='required'>"+
+						"</div>" +
+					"</div>" +
+			  "</div>" 
+		}
+		$("#aaa").html(i);
+		$("#bbb").html(a);
+		laydate.render({
+			elem: '#finishTime'
+		});
+		laydate.render({
+			elem: '#offerTime'
+		});
+	});  
+	
 	form.on("submit(add)", function(data) {
 		var index = top.layer.msg('数据提交中，请稍候', {
 			icon : 16,
@@ -91,4 +130,8 @@ layui.use([ 'layer', 'form' ,'laydate'], function() {
 			});
 		return false;
 	});
+	function graduated(test){
+		alert("djlsdjfsldk");
+	}
 });
+
