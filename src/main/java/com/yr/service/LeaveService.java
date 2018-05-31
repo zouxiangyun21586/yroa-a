e1d7ad59c6381ef6282e4893ce809a7198a2f0e7
“zouxiangyun21586” <a728100142@foxmail.com>
2018-05-31 08:45:11 +0800

    qj

--- src/main/java/com/yr/service/LeaveService.java
+++ src/main/java/com/yr/service/LeaveService.java
@@ -1,7 +1,5 @@
 package com.yr.service;
 
-import java.util.List;
-
 import com.yr.entity.Leave;
 
 /**
@@ -14,45 +12,47 @@ import com.yr.entity.Leave;
  */
 public interface LeaveService {
 	/**
-	 * 添加
+	 * 添加假条
 	 * @author zxy
 	 * 
-	 * 2018年5月23日 上午11:31:04
+	 * 2018年5月23日 上午11:06:13
 	 * 
-	 * @param leave 请假对象
-	 * @return Boolean 以便于判断
+	 * @param leave 假条信息
+	 * @return String 判断是否执行成功
 	 */
-	Boolean add(Leave leave);
+	String add(Leave leave);
+	
+	/**
+	 * 数据回显
+	 * @author zxy
+	 * 
+	 * 2018年5月30日 下午9:42:38
+	 * 
+	 * @param code 学生code
+	 * @return String 判断是否执行成功
+	 */
+	String query(String code);
 
 	/**
-	 * 查询
+	 * 查询所有假条
 	 * @author zxy
 	 * 
-	 * 2018年5月23日 上午11:31:08
-	 * 
-	 * @return Leave的List集合 用于页面显示
+	 * 2018年5月23日 上午11:06:38
+	 * @param page 第几页
+	 * @param limit 每页多少条
+	 * @param name 分页条件
+	 * @return String 判断是否执行成功
 	 */
-	List<Leave> query();
-
-	/**
-	 * 查询指定学生的假条
-	 * @author zxy
-	 * 
-	 * 2018年5月23日 上午11:31:11
-	 * 
-	 * @param studentCode 学生code
-	 * @return 所有值
-	 */
-	List<Leave> query(String studentCode);
+	String query(Integer page, Integer limit, String name);
 
 	/**
 	 * 取消请假
 	 * @author zxy
 	 * 
-	 * 2018年5月23日 上午11:31:13
+	 * 2018年5月23日 上午11:17:00
 	 * 
 	 * @param leave 请假对象
-	 * @return Boolean 便于判断
+	 * @return String 判断是否执行成功
 	 */
-	Boolean cancelLeave(Leave leave);
+	String cancelLeave(Leave leave);
 }
